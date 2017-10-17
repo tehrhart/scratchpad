64bcf6350d6eb2c424a8410ab7e52c1c362096d3
tehrhart <tehrhart@gmail.com>
2017-10-17 09:26:08 +0200

    Update dhcp_vlan_checker.py

--- dhcp_vlan_checker.py
+++ dhcp_vlan_checker.py
@@ -9,7 +9,7 @@ This tool sends out DHCP discovery packets without a 802.1q VLAN header,
 __author__ = "Tim Ehrhart"
 __copyright__ = "Copyright 2017"
 __license__ = "GPL"
-__version__ = "1.0.1"
+__version__ = "1.0.2"
 __maintainer__ = "Tim Ehrhart"
 __email__ = "tehrhart@gmail.com"
 __status__ = "Development"
@@ -18,26 +18,37 @@ from scapy.all import *
 packets = []
 
 def main():
-	if len(sys.argv)!=2:
-		print "%s <interface>" % (sys.argv[0])
+	if len(sys.argv)<2:
+		print "%s version %s by %s (%s)" % (sys.argv[0], __version__, 
+			__author__, __email__)
+		print "Usage: "
+		print "\t%s <interface> [delay]" % (sys.argv[0])
 		sys.exit(1)
 		
 	else:
 		tap_interface = sys.argv[1]
 		conf.checkIPaddr = False
 		fam,hw = get_if_raw_hwaddr(conf.iface)
+		if sys.argv == 3:
+			interval=(sys.argv[2]/1)
+		else:
+			interval=0.002
+		
 		
 		for i in xrange(4095):
 			sendTaggedRequest(i, hw)
 
-		ans, unans = srp(packets, iface=tap_interface, timeout=3, inter=0.002)
+		ans, unans = srp(packets, iface=tap_interface, timeout=3,
+			inter=interval)
 
 		print "\nVLAN\tMAC Address\t\tDHCP Server IP"
 		for p in ans:
 			if Dot1Q in p[1]:
-				print "%d\t%s\t%s" % (p[1][Ether][Dot1Q].vlan, p[1][Ether].src, p[1][IP].src)
+				print "%d\t%s\t%s" % (p[1][Ether][Dot1Q].vlan, 
+					p[1][Ether].src, p[1][IP].src)
 			else:
-				print "(none)\t%s\t%s" % (p[1][Ether].src, p[1][IP].src)	
+				print "(none)\t%s\t%s" % (p[1][Ether].src, 
+					p[1][IP].src)	
 
 
 def sendTaggedRequest(vlanInt, hw):
